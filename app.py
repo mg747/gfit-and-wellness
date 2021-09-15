7c9efbb7547162673c7c072d7db0136549d9f14e
mg747 <morganugiagbe77@gmail.com>
2021-09-15 22:34:14 +0000

    Initial commit

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import os
-from flask import Flask, render_template
+from flask import Flask
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
 
 
+@app.route("/")
+def home():
+    return "Gfit and Wellness!"
+
+
 if __name__ == "__main__":
-    app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")),
-        debug=True)
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
