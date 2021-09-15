dde787a4fc92e2e9163c69ba9b417885762b4f5a
mg747 <morganugiagbe77@gmail.com>
2021-09-15 21:23:14 +0000

    Initial commit wih few lines of code for flask import

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
