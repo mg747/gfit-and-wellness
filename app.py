d4e08f8baa4c4b3b6445bfa9f317b7c5d6ba71db
mg747 <morganugiagbe77@gmail.com>
2021-09-16 23:20:22 +0000

    Add funtions to Connect Flask to Mongodb

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
+mongo = PyMongo(app)
+
+# Diet and recipe section
 @app.route("/")
-def hello():
-    return "Hello World ... again!"
-
-
-if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=True)
-
-
-app = Flask(__name__)
+@app.route("/get_diet")
+def get_diet():
+    diet = mongo.db.diet.find()
+    return render_template("diet.html", diet=diet)
 
 
 if __name__ == "__main__":
