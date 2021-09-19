17bd79f452155a6d6c0ee38eb7378842eff2bf47
mg747 <morganugiagbe77@gmail.com>
2021-09-19 03:04:18 +0000

    Fixed some error and added some style.css file to make the site look more responsive and appealing to the user

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+  // Homepage Carousel function
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+
+  // Homepage Accordion function
+
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
