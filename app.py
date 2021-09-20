1884049c1450ea1a8aa1b7ef505b5db009ab3c0c
mg747 <morganugiagbe77@gmail.com>
2021-09-20 11:00:10 +0000

    Added User Register Functionality

--- app.py
+++ app.py
@@ -64,9 +64,28 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/register")
+# Register
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html", methods=["GET", "POST"])
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
