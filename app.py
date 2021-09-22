0ffe1ee33f7f8840fe1095ce0bd45bcb3bd54755
mg747 <morganugiagbe77@gmail.com>
2021-09-22 06:56:34 +0000

    Add Manage Categories Template

--- app.py
+++ app.py
@@ -146,6 +146,12 @@ def register():
 
     return render_template("register.html")
 
+# Manage Categories
+@app.route("/get_categories")
+def get_categories():
+    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    return render_template("categories.html", categories=categories)
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
